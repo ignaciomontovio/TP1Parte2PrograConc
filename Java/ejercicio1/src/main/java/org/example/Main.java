13dba9d4c6a4e8fa8f5c9a2cc84a56b6bec2f2ec
ignacio montovio <iganciomontovio@gmail.com>
2023-09-14 22:47:27 -0300

    Refactor java proyect.

--- Java/ejercicio1/src/main/java/org/example/Main.java
+++ Java/ejercicio1/src/main/java/org/example/Main.java
@@ -3,68 +3,128 @@ package org.example;
 import java.util.Formatter;
 import java.util.Random;
 
-public class Main {
-    static int[][] matriz1 = new int[5][5];
-    static int[][] matriz2 = new int[5][5];
-    static int[][] matrizResult1 = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
-    static int[][] matrizResult2 = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
+public class Main
+{
 
-    public static void main(String[] args) throws InterruptedException {
-        Random rnd = new Random();
+  static Random rnd = new Random();
+  static final int FIL = 5;
+  static final int COL = 5;
+  static int[][] matriz1 = new int[5][5];
+  static int[][] matriz2 = new int[5][5];
+  static int[][] matrizResultadoCS = new int[5][5];
+  static int[][] matrizResultadoCG = new int[5][5];
 
+  public static void main(String[] args) throws InterruptedException
+  {
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                matriz1[i][j] = (rnd.nextInt(65) - 32);
-                matriz2[i][j] = (rnd.nextInt(65) - 32);
-            }
-        }
+    //Inicilizar matriz con numeros aleatorios
+    inicializarMatriz(matriz1);
+    inicializarMatriz(matriz2);
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                matrizResult1[i][j] = matriz1[i][j] + matriz2[i][j];
-            }
-        }
+    //Mostrar matriz inicial
+    System.out.println("\nMatriz inicial 1:\n");
+    mostrarMatriz(matriz1);
+    System.out.println("\nMatriz inicial 2:\n");
+    mostrarMatriz(matriz2);
 
-        Thread[] threads = new Thread[5];
-        for (int i = 0; i < 5; i++) {
-            threads[i] = new Hilo(i);
-            threads[i].start();
-        }
-        for (int i = 0; i < 5; i++) {
-            threads[i].join();
-        }
+    System.out.println("\n-------------------------------\n");
 
+    //Sumar matriz secuencialmente
+    sumaSecuencial(matriz1, matriz2, matrizResultadoCS);
 
-        System.out.println("Matriz resultado secuencial");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(new Formatter().format("%3d ", matrizResult1[i][j]));
-            }
-            System.out.println();
-        }
-        System.out.println("Matriz resultado con hilos");
-        Formatter format = new Formatter();
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(new Formatter().format("%3d ", matrizResult2[i][j]));
-            }
-            System.out.println();
-        }
+    Thread[] threads = new Thread[5];
+
+    for (int i = 0; i < FIL; i++)
+    {
+      threads[i] = new Hilo(i);
+      threads[i].start();
     }
 
-    public static class Hilo extends Thread {
-        int position;
-        public Hilo(int i) {
-            position= i;
-        }
+    for (int i = 0; i < FIL; i++)
+    {
+      threads[i].join();
+    }
 
-        @Override
-        public void run()
+    System.out.println("Matriz resultado secuencial:\n");
+    mostrarMatriz(matrizResultadoCS);
+
+    System.out.println("\nMatriz resultado con hilos:\n");
+    mostrarMatriz(matrizResultadoCG);
+
+    compararMatriz(matrizResultadoCS, matrizResultadoCG);
+  }
+
+
+  public static class Hilo extends Thread
+  {
+
+    private int position;
+
+    public Hilo(int i)
+    {
+      position = i;
+    }
+
+    public void run()
+    {
+      for (int j = 0; j < COL; j++)
+      {
+        matrizResultadoCG[position][j] = matriz1[position][j] + matriz2[position][j];
+      }
+    }
+
+  }
+
+  private static void inicializarMatriz(int[][] matriz)
+  {
+    for (int i = 0; i < FIL; i++)
+    {
+      for (int j = 0; j < COL; j++)
+      {
+        matriz[i][j] = (rnd.nextInt(65) - 32);
+      }
+    }
+
+  }
+
+  private static void sumaSecuencial(int[][] matriz1, int[][] matriz2, int[][] matrizR)
+  {
+    for (int i = 0; i < FIL; i++)
+    {
+      for (int j = 0; j < COL; j++)
+      {
+        matrizR[i][j] = matriz1[i][j] + matriz2[i][j];
+      }
+    }
+  }
+
+
+  public static void mostrarMatriz(int[][] matriz)
+  {
+    for (int i = 0; i < FIL; i++)
+    {
+      for (int j = 0; j < COL; j++)
+      {
+        System.out.print(new Formatter().format("%3d ", matriz[i][j]));
+      }
+      System.out.println();
+    }
+
+  }
+
+  private static void compararMatriz(int[][] matrizResultadoCS, int[][] matrizResultadoCG)
+  {
+    for (int i = 0; i < FIL; i++)
+    {
+      for (int j = 0; j < COL; j++)
+      {
+        if (!(matrizResultadoCS[i][j] == matrizResultadoCG[i][j]))
         {
-            for (int j = 0; j < 5; j++) {
-                matrizResult2[position][j] = matriz1[position][j] + matriz2[position][j];
-            }
+          System.out.println("Las matrices no son iguales!");
+          System.exit(1);
         }
+      }
     }
+    System.out.println("Las matrices son iguales!");
+  }
 }
